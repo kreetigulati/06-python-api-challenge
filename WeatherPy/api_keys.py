0dd050155b7c041cb503a1e0d2325571be5934d4
kreetigulati <kreetigulati@outlook.com>
2021-05-15 21:34:28 -0700

    Final Python APIs homework

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f08e57d65f6c7be2fed45791a39675ba"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAhWW8CyjyCy-YAacRBAkcXe5ATEBpuMT0"
